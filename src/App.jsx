3b03535bda36106320e646f8ca2e6869ed9deb36
PepeTru3no <jvinetconsola@gmail.com>
2024-11-25 20:27:35 -0300

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { pizzas } from './data/pizzas';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [productos, setProductos]= useState(["Producto 1", "Producto 2", "Producto 3", "Producto 4"]);
+  const [pizas, setPizas] = useState(pizzas);
+  const [productoNuevo, setProductoNuevo] = useState("");
+
+  const agregarProducto=(e)=>{
+    e.preventDefault();
+    if(!productoNuevo.trim()){
+      alert('debe agregar algun producto')
+      return
+    }
+    setProductos([...productos, productoNuevo]);
+    setProductoNuevo("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form onSubmit={agregarProducto}>
+    <input type="text" name="productoNuevo" onChange={(e)=>setProductoNuevo(e.target.value)} value={productoNuevo} />
+    <button type="submit">Agregar Producto</button>
+    </form>
+    <ul>
+      {productos.map((prodcuto, key)=>{
+        return <li key={key}>{prodcuto}</li>
+      })}
+    </ul>
+      
+      
+      
+      <div style={{display:'grid'}}>
+        {pizas.map((pizza, key)=>{
+          return(
+            <div style={{display:'flex', flexDirection:'column'}} key={key} >
+              <h3> {pizza.name}</h3>
+              <img src={pizza.img} alt="" />
+              <p>{pizza.desc}</p>
+              <ul>
+                {pizza.ingredients.map((ingredient, key)=>{
+                  return <li key={key}>{ingredient}</li>
+                })}
+              </ul>
+              <p>Precio: ${pizza.price}</p>
+
+            </div>
+          ) 
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
